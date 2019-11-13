4b38ab74feda5f6cad14544ff69cae6603a920d1
Sammy <samantha.yiu@gmail.com>
2019-11-13 16:08:11 -0500

    Add sunset

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
             callback(undefined, {
                 summary: body.daily.data[0].summary,
                 temp: body.currently.temperature,
-                chanceofrain: body.currently.precipProbability.body,
+                chanceofrain: body.currently.precipProbability,
                 sunset: body.daily.data[0].sunsetTime
             })
         }
